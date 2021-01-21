a264da0a94a3b96fdd25b7caad0d25465c870957
adam stevenson <adamscdcm@gmail.com>
2021-01-21 13:24:27 -0800

    made corrections from feedback

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ var specialChar = "!@#$%^&*()?/;:~";
 var userSelection =[]; 
 
 var finalPassword = [];
-  //console.log("finalPassword =" + finalPassword);
+//console.log("finalPassword =" + finalPassword);
 
 //added functions, prompt, console.log (1 removed perams passlength)
 function generatePassword() { 
@@ -27,41 +27,48 @@ function generatePassword() {
   return;
   }
   
+  console.log("passlenght: " + passLength);
+
   //added upperCase/lowerCase confirm var
   var upperC = confirm("Would you like Uppercase Letters");
   if(upperC) {
-    userSelection.push(upperCase);
+    userSelection.push(...upperCase);
   } 
+  console.log("uppercase = " + userSelection);
   
   var lowerC = confirm("Would you like Lowercase Letters");
   if(lowerC) {
-    userSelection.push(lowerCase);
+    userSelection.push(...lowerCase);
   }
+  console.log("lowercase+uppercase = " + userSelection);
+
   //added remaining specChar and numb var
   var specC = confirm("Would you like and Special Characters");
   if(specC) {
-    userSelection.push(specialChar);
+    userSelection.push(...specialChar);
   }
+  console.log("special+lower+upper = " + userSelection);
 
   var numb = confirm("Would you like to add Numbers to your password?"); 
   if(numb) { 
-    userSelection.push(numbers);
+    userSelection.push(...numbers);
   }
-  console.log("user selection = " + userSelection);
+  console.log("special+number+upper+lower = " + userSelection);
   //console.log("final Password = " + finalPassword);
 
   //not sure if this statement is needed-- if (upperC != true && lowerC != true && specC != true && )
   
-  for (var i = 0; i < passLength; i++) {
+  for (let i = 0; i < passLength; i++) {
+    console.log("passlength[i]: "+ passLength);
     finalPassword.push(userSelection[Math.floor(Math.random() * userSelection.length)]);
-      console.log(finalPassword.join('+')); 
-    var randomIndex = Math.floor(Math.random() * passLength);
-    var random = passLength.charAt(randomIndex);
-    console.log(random); 
+    console.log("final password: "+finalPassword.join("+")); 
+    // var randomIndex = Math.floor(Math.random() * passLength);
+    // var random = passLength.charAt(randomIndex);
+    // console.log(random); 
   }
 
-    document.getElementById('password').value = finalPassword.join('');
-    alert('Your new Password is: ' + finalPassword.join(""));
+  document.getElementById('password').value = finalPassword.join("");
+  alert('Your new Password is: ' + finalPassword.join(""));
   
   //var math.floor will pick a random index for userSelection array set to var
   //math.floor to pick random char  --name as var use this to concat
@@ -80,5 +87,5 @@ function generatePassword() {
 //}
 
 // Add event listener to generate button
-var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", generatePassword);
+// var generateBtn = document.querySelector("#generate");
+// generateBtn.addEventListener("click", generatePassword);
